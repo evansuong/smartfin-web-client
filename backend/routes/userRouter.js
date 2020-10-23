2ed3ca51ec5eb3292122ba2138ecc786ba0f2417
Anshul-Birla <anshulbirla@gmail.com>
2020-10-22 17:18:55 -0700

    misc. changes

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const functions = require("../functions/registration_functions");
+
+
 router.post("/register", async (req, res) => {
   try {
     const { email, password, passwordCheck, displayName } = req.body;
