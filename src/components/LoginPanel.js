5a7264eb6ab8b3dd459e97358ffa397f3f1bf8de
tbdoan <tbdoan@ucsd.edu>
2020-10-28 17:22:26 -0700

    styles on login panel

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FormControl, InputLabel, Input, Button } from '@material-ui/core';
 import '../styles/LoginPanel.css';
 
